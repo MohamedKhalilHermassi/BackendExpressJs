d0d0b713fe814d11d5d5db6d1ef433ac13041645
AyoubHdhili <hdhili.ayoub@esprit.tn>
2024-03-31 00:54:03 +0100

    correct location model

--- routes/locationRouter.js
+++ routes/locationRouter.js
@@ -12,6 +12,8 @@ router.post('/', async (req, res, next) => {
         state : req.body.state,
         city : req.body.city,
         address : req.body.address,
+        lat: req.body.lat,
+        lng: req.body.lng,
         numberOfFloors : req.body.numberOfFloors
     })
     
